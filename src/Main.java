4453fabcef9d5814fb491b3d60fc68d0d8f4d1c3
Stanislaw <stas33933@gmail.com>
2025-09-18 18:46:33 +0300

    Добавил Library Manager, Можно найти книгу по id и добавить книгу куда угодно

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,18 @@ public class Main {
         Shelf shelf1 = new Shelf(1);
 
         Book book1 = new Book("Ракамакафо", "Бочаров С. С.", 230, 1);
+        Book book2 = new Book("Таракан", "Гыгун Р. А.", 100, 2);
+
 
         shelf1.addBook(book1);
         closet1.addShelf(shelf1);
         room1.addCloset(closet1);
 
-        Book found = room1.findClosetById(1).findShelfById(1).findBookById(1);
-        System.out.println(found);
+        LibraryManager libman = new LibraryManager();
+        libman.addRoom(room1);
+
+        System.out.println(libman.findBookEverywhere(1));
+        libman.addBookToLocation(book2, 1, 1, 1);
+        System.out.println(libman.findBookEverywhere(2));
     }
 }
