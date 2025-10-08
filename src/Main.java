5811f2cabe31c62366a880a13f49e01c83ae2889
Stanislaw <118082444+exzoe@users.noreply.github.com>
2025-10-08 14:57:14 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         LibraryManager libraryManager = new LibraryManager();
 
         Room room1 = new Room(1);
